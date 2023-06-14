bb0bfa2ef00e0ed5a258250a03b59c215e4f7d88
Flávia Costa <flavia.cbrasileiro@gmail.com>
2023-06-14 19:04:50 -0300

    atualizaçao produto

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const cartItems = []
+
 let isTotalHidden = true;
+
 const backpack = {
     name: "Backpack",
-    price: 400
+    price: 200
 };
 
 const camera = {
     name: "Camera",
-    price: 300
+    price: 450
 };
+
+const barraca = {
+    name: "Barraca",
+    price: 800
+};
+
+function showTotal() {
+    const orderTotal = document.getElementById("orderTotal");
+    orderTotal.innerHTML = "";
+
+    if (isTotalHidden === false) {
+        let total = 0;
+
+        for (let i = 0; i < cartItems.length; i++) {
+            total  += cartItems[i].price;
+            //total = total + cartItems[i].price;
+        }
+        orderTotal.innerHTML += "Total: $" + total;
+
+    }
+    
+}
 
 function addToCart(item) {
     cartItems.push(item);
@@ -24,30 +48,4 @@ function clickCart() {
     
 }
 
-function showTotal() {
-    const orderTotal = document.getElementById("orderTotal");
-    orderTotal.innerHTML = "";
-
-    if (isTotalHidden === false) {
-        let total = 0;
-
-        for (let i = 0; i < cartItems.length; i++) {
-            total  += cartItems[i].price;
-            
-        }
-        orderTotal.innerHTML += "Total: $" + total;
-    }
-
-
-}
-
-function calculateTotal(total) {
-    let shipping;
-    if (total >= 700) {
-        shipping = 30;   
-    } else {
-        shipping = 50;
-    }
-    return shipping;   
-}
 
