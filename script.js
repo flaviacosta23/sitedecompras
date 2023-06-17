6e0250d0fb5fae0091f470f5a234381302977609
Flávia Costa <flavia.cbrasileiro@gmail.com>
2023-06-16 22:23:03 -0300

    melhoria no layout e atualização das funções

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const barraca = {
     price: 800
 };
 
-function showTotal() {
+function showTotal() { //essa função define o subtotal, o frete e o valor somado
     const orderTotal = document.getElementById("orderTotal");
     orderTotal.innerHTML = "";
 
@@ -25,16 +25,26 @@ function showTotal() {
         let total = 0;
 
         for (let i = 0; i < cartItems.length; i++) {
-            total  += cartItems[i].price;
-            //total = total + cartItems[i].price;
+            total  += cartItems[i].price; // loop para calcular somatorio dos produtos comprados
+
         }
-        orderTotal.innerHTML += "Total: $" + total;
+        orderTotal.innerHTML = "Subtotal: $" + total; // subtotal dos produtos comprados
+
+        const enviar = document.getElementById("envio");
+        enviar.innerHTML = "Envio: $" + calculateTotal(total); //valor do frete
+        
+
+        let soma = total + calculateTotal(total);
+
+        const totalEnvio = document.getElementById("pedidoFinal");
+        totalEnvio.innerHTML = "Total: $" + soma; // valor total = subtotal + frete
 
     }
+
     
 }
 
-function addToCart(item) {
+function addToCart(item) { //função para add o produto no carrinho
     cartItems.push(item);
 
     document.getElementById("itemCounter").innerHTML = cartItems.length;
@@ -42,10 +52,24 @@ function addToCart(item) {
     
 }
 
-function clickCart() {
-    isTotalHidden = !isTotalHidden;
+function clickCart() { // funçao para esconder/mostrar os valores do carrinho
+    isTotalHidden = !isTotalHidden; //ao ativar a função o valor novo é o antigo negado
     showTotal();
-    
+
 }
 
+
+
+function calculateTotal(totalvar) { //função que calcula o valor do frete / se nao tiver produto, frete igual a zero
+    let shipping;
+    if (totalvar == 0) {
+        shipping = 0;
+    }
+    else if (totalvar >= 700) {
+        shipping = 30;
+    } else {
+        shipping = 50;
+    }
+    return shipping;
+}
 
